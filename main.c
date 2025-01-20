18e0d4998aa06d831ec5c155606821138f589486
mmmharius <mariuspapin@rcmm.fr>
2025-01-20 18:42:56 +0100

    parsing

--- main.c
+++ main.c
@@ -12,23 +12,53 @@
 
 #include "libft.h"
 
-void    cmd(char *argv)
+char    **get_path(char **env)
 {
-    char **cmd;
-    int i = 0;
+    int i;
+    char *path_env;
+    char **all_path;
 
-    cmd = ft_split(argv, ' ');
-    while (cmd[i] != NULL)
+    path_env = NULL;
+    all_path = NULL;
+    i = 0;
+    while(env[i])
     {
-        printf("%s", cmd[i]);
+        if (ft_strnstr(env[i], "PATH=", 5))
+            path_env = env[i] + 5;
         i++;
     }
-    return;
+    if (!path_env)
+        printf("Error\nPATH pas recupere/trouve");
+    all_path = ft_split(path_env, ':');
+    if (!all_path)
+        printf("Error\nsplit a fail");
+    return(all_path);
 }
 
-int main(int argc, char **argv)
+void    get_cmd_path(char **all_path, char *cmd)
+{
+    char **all_cmd;
+    char *cmd_path;
+    int i;
+
+    i = 0;
+    all_cmd = ft_split(cmd, ' ');
+    while(all_path[i])
+    {
+        cmd_path = ft_strjoin(all_path[i], "/");
+        cmd_path = ft_strjoin(cmd_path, all_cmd[0]);
+        if (access(cmd_path, F_OK) == 0 && access(cmd_path, X_OK) == 0)
+        {
+            printf("TROUVER");
+            return ;
+        }
+        i++;
+    }
+}
+
+int main(int argc, char **argv, char **env)
 {
     if (argc != 5)
         return(1);
-    cmd(argv[2]);
+    get_cmd_path(get_path(env), argv[2]);
 }
